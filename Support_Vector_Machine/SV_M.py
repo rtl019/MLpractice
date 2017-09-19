4fe1cc6fdd725d9530053ef511db257258f89714
Ratul <mislam@wpi.edu>
2017-09-18 22:32:38 -0400

    Second Commit restructuring and starting the project

---
+++ Support_Vector_Machine/SV_M.py
@@ -0,0 +1,9 @@
+import sys
+#warning append the directory where you have your data
+#it is the machine where the author worked on specific 
+sys.append.path("C:/Users/Ratul/Desktop/nadiabot") 
+
+from matplotlib import pyplot as plt
+
+import numpy as np
+
