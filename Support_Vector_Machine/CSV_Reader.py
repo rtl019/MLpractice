bea98c79e02d4e59eb2d4f53a55e9b3dbf3d5cc4
Ratul <mislam@wpi.edu>
2017-09-18 22:44:44 -0400

    Commit : 3 making the directory structure work

---
+++ Support_Vector_Machine/CSV_Reader.py
@@ -0,0 +1,61 @@
+import csv
+def Reader(fileName):
+    file=open(fileName)
+    dicr=csv.DictReader(file)
+    for row in dicr:
+        L=[]
+        L.append(row['price'])
+        print(row('price'))    
+    
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""def reader(fileName):
+    file=open(fileName)
+    L=[]
+    M=[]
+    L1=[]
+    L2=[]
+    dicr=csv.DictReader(file)
+    for row in dicr:
+        L.append(row['price'])
+        L1.append(row['latitude'])
+        L2.apprnd(row['longitude'])
+        
+    l1=len(L1)
+    l2=len(L2)
+    a=len(L)
+    #return(a)
+    for p in range(a):
+        d=eval(L[p])
+        M.append(d)
+    f=len(M)
+    for i in range(f):
+        if(M[i]<100000):
+            r=
+    
+    Maxmin=[]
+    for i in range(0,J):
+        d=eval(M[i])
+        Maxmin.append(d)
+        s=s+d
+    m=max(Maxmin)
+    n=min(Maxmin)
+    #print(s)
+    Average= (s/J)
+    print("Average is ",Average)
+    print("Maximum Grade is",m)
+    print("Lowest Grade is ",""""     
--- Support_Vector_Machine/SV_M.py
+++ Support_Vector_Machine/SV_M.py
@@ -1,9 +1,21 @@
 import sys
 #warning append the directory where you have your data
 #it is the machine where the author worked on specific 
-sys.append.path("C:/Users/Ratul/Desktop/nadiabot") 
+sys.path.append("C:/Users/Ratul/Desktop/nadiabot") 
 
 from matplotlib import pyplot as plt
 
 import numpy as np
 
+import csv 
+
+
+
+
+def main(): 
+	print("Hi machine learning from Python")
+
+
+
+if __name__ == '__main__':
+	main()
