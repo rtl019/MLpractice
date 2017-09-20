12ff76f37458ddb40f854b8302f57c528b215dee
Ratul <mislam@wpi.edu>
2017-09-19 23:53:31 -0400

    name changes and some editst

---
+++ regression_analysis/linear_regression.py
@@ -0,0 +1,24 @@
+import sys
+#warning append the directory where you have your data
+#it is the machine where the author worked on specific 
+sys.path.append("C:/Users/Ratul/Desktop/nadiabot") 
+
+from matplotlib import pyplot as plt
+
+import numpy as np
+
+from CSV_Reader import *
+
+#bunch of imports basically dependencies of this basic script 
+
+
+def main(): 
+	print("Hi machine learning from Python")
+	file=input("CSV file from where the data needs to be read : ")
+	price=Reader(file)
+	print(type(price)) 
+
+
+
+if __name__ == '__main__':
+	main()
